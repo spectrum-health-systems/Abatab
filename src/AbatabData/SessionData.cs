22ac679bef336605ccfed386455cc391f5d8238a
Chris Banwarth <github@aprettycoolprogram.com>
2022-09-27 11:21:44 -0400

    v0.5.0

---
+++ src/AbatabData/SessionData.cs
@@ -0,0 +1,31 @@
+﻿/* ========================================================================================================
+ * Abatab v0.5.0
+ * https://github.com/spectrum-health-systems/Abatab
+ * (c) 2021-2022 A Pretty Cool Program (see LICENSE file for more information)
+ * --------------------------------------------------------------------------------------------------------
+ * AbatabData.SessionData.cs v0.5.0-development+220927.110333
+ * https://github.com/spectrum-health-systems/Abatab/blob/main/doc/srcdoc/SrcDocAbatabData.md
+ * ===================================================================================================== */
+
+using NTST.ScriptLinkService.Objects;
+
+namespace AbatabData
+{
+    public class SessionData
+    {
+        // web.config settings.
+        public string AbatabMode { get; set; }
+        public string LogMode { get; set; }
+        public string AbatabRootDirectory { get; set; }
+        public string AvatarFallbackUserName { get; set; }
+
+        // Runtime settings.
+        public string DateStamp { get; set; }
+        public string TimeStamp { get; set; }
+        public string AvatarUserName { get; set; }
+        public string AbatabRequest { get; set; }
+        public OptionObject2015 SentOptObj { get; set; }
+        public OptionObject2015 WorkerOptObj { get; set; }
+        public OptionObject2015 FinalOptObj { get; set; }
+    }
+}
---
+++ src/AbatabLogging/WriteFile.cs
@@ -0,0 +1,15 @@
+﻿/* ========================================================================================================
+ * Abatab v0.5.0
+ * https://github.com/spectrum-health-systems/Abatab
+ * (c) 2021-2022 A Pretty Cool Program (see LICENSE file for more information)
+ * --------------------------------------------------------------------------------------------------------
+ * AbatabLogging.WriteFile.cs v0.5.0-development+220927.110333
+ * https://github.com/spectrum-health-systems/Abatab/blob/main/doc/srcdoc/SrcDocAbatabLogging.md
+ * ===================================================================================================== */
+
+namespace AbatabLogging
+{
+    internal class WriteFile
+    {
+    }
+}
