0a5f6fc6bbc84a986c20332e41dc526908d721e9
Chris Banwarth <github@aprettycoolprogram.com>
2022-09-28 17:30:13 -0400

    Update

--- src/Abatab.asmx.cs
+++ src/Abatab.asmx.cs
@@ -38,7 +38,7 @@ namespace Abatab
         [WebMethod]
         public OptionObject2015 RunScript(OptionObject2015 sentOptionObject, string abatabRequest)
         {
-            File.WriteAllText(@"C:\AvatoolWebService\AbatabData\log.txt", $"Request: {abatabRequest}");
+            File.WriteAllText(@"C:\AvatoolWebService\Abatab_Logs\log.txt", $"Request: {abatabRequest}");
 
             //var abatabSession = Instance.Build(sentOptionObject, abatabRequest);
             //LogEvent.Trace(Assembly.GetExecutingAssembly().GetName().Name, abatabSession);
