fd16a8b50031fa88fae9d7d4b751e902fc7427ba
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-03 08:47:53 -0400

    v0.90.1-devbuild+221003.084751

--- src/AbatabLogging/LogEvent.cs
+++ src/AbatabLogging/LogEvent.cs
@@ -40,7 +40,7 @@ namespace AbatabLogging
                 var debugLogDir = $@"{debugModuleDir}\{DateTime.Now:yyMMdd}";
                 _=Directory.CreateDirectory(debugLogDir);
 
-                File.WriteAllText($@"{debugLogDir}\{DateTime.Now.ToString("HHmmss.fffffff")}.{executingAssemblyName}", debugMsg);
+                File.WriteAllText($@"{debugLogDir}\{DateTime.Now.ToString("HHmmss.fffffff")}.{executingAssemblyName}", executingAssemblyName);
             }
         }
 
