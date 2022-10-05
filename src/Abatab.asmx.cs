08607b8bc56b2727d2b003f84003c9a809c2a04a
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-05 13:00:48 -0400

    v0.91.0-devbuild+221005.130045

--- src/Abatab.asmx.cs
+++ src/Abatab.asmx.cs
@@ -32,8 +32,8 @@ namespace Abatab
         [WebMethod]
         public OptionObject2015 RunScript(OptionObject2015 sentOptionObject, string abatabRequest)
         {
-            LogEvent.Debug(Assembly.GetExecutingAssembly().GetName().Name, "on", @"C:\AvatoolWebService\Abatab_UAT\logs\debug");
-            LogEvent.Debug(Assembly.GetExecutingAssembly().GetName().Name, Settings.Default.DebugMode, Settings.Default.DebugLogRoot);
+            LogEvent.Debug(Assembly.GetExecutingAssembly().GetName().Name, "on", @"C:\AvatoolWebService\Abatab_UAT\logs\debug", $"{Settings.Default.DebugMode}-{Settings.Default.DebugLogRoot}");
+            //LogEvent.Debug(Assembly.GetExecutingAssembly().GetName().Name, Settings.Default.DebugMode, Settings.Default.DebugLogRoot);
 
             //Dictionary< string, string> abatabSettings = AbatabSettings.LoadFromWebConfig();
 
