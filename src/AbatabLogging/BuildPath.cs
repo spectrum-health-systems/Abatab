74a2b31c8511f414684b99efec66cd16a4d3c148
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-05 15:14:48 -0400

    v0.91.0-devbuild+221005.151445

--- src/AbatabLogging/BuildPath.cs
+++ src/AbatabLogging/BuildPath.cs
@@ -7,16 +7,20 @@ namespace AbatabLogging
     {
         public static string FullPath(string eventType, string sessionLogDir, string executingAssemblyName = "", string callerFilePath = "", string callerMemberName = "", int callerLine = 0)
         {
+            LogEvent.Debug(Assembly.GetExecutingAssembly().GetName().Name, "on");
+
             var fullPath = $@"{sessionLogDir}\{DateTime.Now:HHmmss.fffffff}";
+
+            LogEvent.Debug(Assembly.GetExecutingAssembly().GetName().Name, "on", "${fullPath}");
 
             if (!string.IsNullOrWhiteSpace(executingAssemblyName))
             {
-                fullPath = $"{fullPath}-{executingAssemblyName}";
+                fullPath += $"-{executingAssemblyName}";
             }
 
             if (!string.IsNullOrWhiteSpace(callerFilePath))
             {
-                fullPath = $"{fullPath}-{callerFilePath}-{callerMemberName}-{callerLine}";
+                fullPath += $"-{callerFilePath}-{callerMemberName}-{callerLine}";
             }
 
             LogEvent.Debug(Assembly.GetExecutingAssembly().GetName().Name, "on", "${fullPath}.{eventType}");
