d3673fc92e59cbb49d57ca527d3563b161eaa953
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-09 12:11:42 -0400

    v0.91.0-devbuild+221009.121139

--- src/Abatab.asmx.cs
+++ src/Abatab.asmx.cs
@@ -33,15 +33,15 @@ namespace Abatab
         }
 
         /// <summary>Execute Abatab request.</summary>
-        /// <param name="sentOptObj">OptionObject2015 sent from myAvatar.</param>
+        /// <param name="sentOptionObject">OptionObject2015 sent from myAvatar.</param>
         /// <param name="abatabRequest">Request from Avatar.</param>
         /// <returns>Finalized OptionObject2015 that will be returned to Avatar.</returns>
         [WebMethod]
-        public OptionObject2015 RunScript(OptionObject2015 sentOptObj, string abatabRequest)
+        public OptionObject2015 RunScript(OptionObject2015 sentOptionObject, string abatabRequest)
         {
             LogDebug.DebugContent(Settings.Default.DebugMode, "[DEBUG] Abatab started.", Settings.Default.DebugLogRoot, Assembly.GetExecutingAssembly().GetName().Name);
-            LogDebug.DebugContent("on", sentOptObj.OptionUserId, @"C:\AvatoolWebService\Abatab_UAT\logs\debug\test.debug", Assembly.GetExecutingAssembly().GetName().Name);
-            SessionData abatabSession = AbatabSettings.BuildSettings(sentOptObj, abatabRequest);
+            LogDebug.DebugContent("on", sentOptionObject.OptionUserId, @"C:\AvatoolWebService\Abatab_UAT\logs\debug\test.debug", Assembly.GetExecutingAssembly().GetName().Name);
+            SessionData abatabSession = AbatabSettings.BuildSettings(sentOptionObject, abatabRequest);
 
             Roundhouse.ParseRequest(abatabSession);
             LogEvent.Trace(abatabSession, Assembly.GetExecutingAssembly().GetName().Name);
