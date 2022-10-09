45362203c5f3182466a7d5fba4ebcccb3cee4b43
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-09 15:05:56 -0400

    Refresh repository

---
+++ src/AbatabLogging/BuildPath.cs
@@ -0,0 +1,51 @@
+﻿/* ========================== https://github.com/spectrum-health-systems/Abatab ===========================
+ * Abatab                                                                                           v0.91.0
+ * AbatabLogging.csproj                                                                             v0.91.0
+ * BuildPath.cs                                                                              b221009.083236
+ * ================================ (c) 2016-2022 A Pretty Cool Program ================================ */
+
+/* Builds the log file path.
+ */
+
+using System;
+using System.IO;
+
+namespace AbatabLogging
+{
+    public class BuildPath
+    {
+
+        /// <summary></summary>
+        /// <param name="eventType"></param>
+        /// <param name="sessionLogDir"></param>
+        /// <param name="executingAssemblyName"></param>
+        /// <param name="callerFilePath"></param>
+        /// <param name="callerMemberName"></param>
+        /// <param name="callerLine"></param>
+        /// <returns></returns>
+        public static string FullPath(string eventType, string sessionLogDir, string executingAssemblyName = "", string callerFilePath = "", string callerMemberName = "", int callerLine = 0)
+        {
+            var fullPath = sessionLogDir;
+
+            if (eventType == "trace")
+            {
+                fullPath += $@"\{eventType}";
+                AbatabSystem.Maintenance.VerifyDir(fullPath);
+            }
+
+            fullPath += $@"\{DateTime.Now:HHmmss.fffffff}";
+
+            if (!string.IsNullOrWhiteSpace(executingAssemblyName))
+            {
+                fullPath += $"-{executingAssemblyName}";
+            }
+
+            if (!string.IsNullOrWhiteSpace(callerFilePath))
+            {
+                fullPath += $"-{Path.GetFileName(callerFilePath)}-{callerMemberName}-{callerLine}";
+            }
+
+            return $"{fullPath}.{eventType}";
+        }
+    }
+}
