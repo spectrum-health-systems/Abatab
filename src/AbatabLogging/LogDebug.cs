da4855145a881174227b7f9ff044aa019693563a
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-09 15:21:06 -0400

    v0.91.0-devbuild+221009.152104

--- src/AbatabLogging/LogDebug.cs
+++ src/AbatabLogging/LogDebug.cs
@@ -1,10 +1,30 @@
 ﻿/* ========================== https://github.com/spectrum-health-systems/Abatab ===========================
  * Abatab                                                                                           v0.91.0
  * AbatabLogging.csproj                                                                             v0.91.0
- * Debugger.cs                                                                               b221009.083236
- * ================================ (c) 2016-2022 A Pretty Cool Program ================================ */
+ * LogDebug.cs                                                                               b221009.090325
+ * --------------------------------------------------------------------------------------------------------
+ * Build and write a debug log file.
+ * ================================= (c)2016-2022 A Pretty Cool Program ================================ */
 
-/* Logic for debugging logs.
+/* Abatab debugging functionality should only be used for development/debugging. There is a hardcoded 100ms
+ * delay when writing a debug log file, and there for should not be used production environments due to
+ * the performance penalties it creates.
+ *
+ * The "DebugMode" setting in Web.config should be set to "off" unless you are debugging the Abatab source
+ * code. Debug logs will only be written if DebugMode is set to "on".
+ *
+ * The advantage of debug logs is that you can create them at any time, even prior to the creation of a
+ * SessionData object. All you need to do is add the following line anywhere in your code:
+ *
+ *      LogDebug.Debug()
+ *
+ * This will create a basic debug log file in the following hardcoded location:
+ *
+ *      C:\AvatoolWebService\Abatab_UAT\logs\debug\%yyMMdd%\%HHmmssfffffff%-%executingAssembly%-%callerPath%}-%callerMember%-%callerLine%.debug"
+ *
+ * You can also debug the debugger by setting "debugDebugger" to "true" in LogDebug.DebugContent(), which
+ * will create additional debug files to aid in troubleshooting. These files have an additional hardcoded
+ * 1000ms delay when they are written, and will cause significant performance issues when Abatab executes.
  */
 
 using System;
@@ -16,21 +36,17 @@ namespace AbatabLogging
 {
     public class LogDebug
     {
-        /// <summary>Basic debugging for a module.</summary>
-        /// <param name="exeAssembly"></param>
-        /// <param name="debugMode"></param>
-        /// <param name="debugLogRoot"></param>
-        /// <param name="debugMsg"></param>
-        /// <param name="callPath"></param>
-        /// <param name="callMember"></param>
-        /// <param name="callLine"></param>
+        /// <summary>Basic debugging.</summary>
+        /// <param name="debugMode">Debug log mode.</param>
+        /// <param name="debugMsg">Debug log message.</param>
+        /// <param name="debugLogRoot">Debug log root directory.</param>
+        /// <param name="exeAssembly">Name of executing assembly.</param>
+        /// <param name="callerPath">Filename of where the log is coming from.</param>
+        /// <param name="callerMember">Method of where the log is coming from.</param>
+        /// <param name="callerLine">File line of where the log is coming from.</param>
         public static void DebugContent(string exeAssembly, string debugMode, string debugLogRoot = "", string debugMsg = "", [CallerFilePath] string callPath = "", [CallerMemberName] string callMember = "", [CallerLineNumber] int callLine = 0)
         {
-            /* NOTE The advantage of debug logs is that they can be created prior to a SessionData object being created. You can put a LogDebug.Debug() call
-             * anywhere in your code, and a logfile will be written. This is for development/debugging purposes, therefore the DebugMode setting in Web.config
-             * should be set to "off" in your production environment.
-             */
-
+            //if (debugMode == "on" || debugMode == "undefined") // TODO Remove.
             if (debugMode == "on" || debugMode == "undefined")
             {
                 const bool debugDebugger = false;
