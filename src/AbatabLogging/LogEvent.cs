c6faeae99300c11665742443cb7d7a8c62681b07
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-10 08:53:55 -0400

    Revert

---
+++ src/AbatabLogging/LogEvent.cs
@@ -0,0 +1,66 @@
+﻿/* ========================== https://github.com/spectrum-health-systems/Abatab ===========================
+ * Abatab                                                                                           v0.91.0
+ * AbatabLogging.csproj                                                                             v0.91.0
+ * LogEvent.cs                                                                               b221009.090325
+ * --------------------------------------------------------------------------------------------------------
+ * Build a log files.
+ * ================================= (c)2016-2022 A Pretty Cool Program ================================ */
+
+using AbatabData;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace AbatabLogging
+{
+    public class LogEvent
+    {
+        /// <summary>Build a OptionObject information log.</summary>
+        /// <param name="abatabSession"></param>
+        /// <param name="logMsg"></param>
+        public static void AllOptionObjectInformation(SessionData abatabSession, string logMsg = "OptionObject information log.")
+        {
+            BuildContent.LogComponents("allOptObjInformation", abatabSession, logMsg);
+        }
+
+
+        /// <summary>Build a session detail log.</summary>
+        /// <param name="abatabSession">Abatab session configuration settings.</param>
+        /// <param name="logMsg">Message for the logfile</param>
+        public static void SessionDetail(SessionData abatabSession, string logMsg = "Session detail log.")
+        {
+            LogDebug.DebugContent(Assembly.GetExecutingAssembly().GetName().Name, abatabSession.DebugMode, abatabSession.DebugLogRoot, "[DEBUG] Creating session log.");
+            LogEvent.Trace(abatabSession, Assembly.GetExecutingAssembly().GetName().Name, abatabSession.FinalOptObj.ErrorMesg);
+
+            if (abatabSession.LoggingMode == "all" || abatabSession.LoggingMode.Contains("session"))
+            {
+                LogEvent.Trace(abatabSession, Assembly.GetExecutingAssembly().GetName().Name, abatabSession.FinalOptObj.ErrorMesg);
+                var logPath    = BuildPath.FullPath("session", abatabSession.SessionLogRoot);
+                var logContent = BuildContent.LogComponents("sessionInformation", abatabSession, logMsg);
+
+                WriteFile.LocalFile(logPath, logContent, Convert.ToInt32(abatabSession.LoggingDelay));
+            }
+
+        }
+
+        /// <summary>Build a trace log.</summary>
+        /// <param name="exeAssembly">Name of executing assembly.</param>
+        /// <param name="abatabSession">Abatab session configuration settings.</param>
+        /// <param name="logMsg">Message for the logfile</param>
+        /// <param name="callerPath">Filename of where the log is coming from.</param>
+        /// <param name="callerMember">Method of where the log is coming from.</param>
+        /// <param name="callerLine">File line of where the log is coming from.</param>
+        public static void Trace(SessionData abatabSession, string exeAssembly, string logMsg = "Trace log start...", [CallerFilePath] string callPath = "", [CallerMemberName] string callMember = "", [CallerLineNumber] int callLine = 0)
+        {
+            LogDebug.DebugContent(Assembly.GetExecutingAssembly().GetName().Name, abatabSession.DebugMode, abatabSession.DebugLogRoot, "[DEBUG] Creating trace log.");
+
+            if (abatabSession.LoggingMode == "all" || abatabSession.LoggingMode.Contains("trace"))
+            {
+                var logPath    = BuildPath.FullPath("trace", abatabSession.SessionLogRoot, exeAssembly, callPath, callMember, callLine);
+                var logContent = BuildContent.LogComponents("trace", abatabSession, logMsg, exeAssembly, callPath, callMember, callLine);
+
+                WriteFile.LocalFile(logPath, logContent, Convert.ToInt32(abatabSession.LoggingDelay));
+            }
+        }
+    }
+}
---
+++ src/AbatabSettings.cs
@@ -0,0 +1,56 @@
+﻿/* ========================== https://github.com/spectrum-health-systems/Abatab ===========================
+ * Abatab                                                                                           v0.91.0
+ * Abatab.csproj                                                                                    v0.91.0
+ * AbatabSettings.cs                                                                         b221009.090325
+ * --------------------------------------------------------------------------------------------------------
+ * Contains the logic to work with configuration settings found in the local Web.Config file.
+ * ================================= (c)2016-2022 A Pretty Cool Program ================================ */
+
+using Abatab.Properties;
+using AbatabData;
+using AbatabLogging;
+using AbatabSession;
+using NTST.ScriptLinkService.Objects;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Abatab
+{
+    public class AbatabSettings
+    {
+        /// <summary>Build the abatabSession object</summary>
+        /// <param name="sentOptObj">OptionObject2015 sent from myAvatar.</param>
+        /// <param name="abatabSession">Abatab session data.</param>
+        /// <returns>Completed abatabSession object.</returns>
+        public static SessionData BuildSettings(OptionObject2015 sentOptObj, string abatabRequest)
+        {
+            LogDebug.DebugContent(Assembly.GetExecutingAssembly().GetName().Name, Settings.Default.DebugMode, Settings.Default.DebugLogRoot, "[DEBUG] Building Abatab session settings.");
+
+            Dictionary<string, string> abatabSettings = AbatabSettings.LoadWebConfig();
+
+            return Instance.Build(sentOptObj, abatabRequest, abatabSettings);
+        }
+
+        /// <summary>Load local configuration settings from Web.config.</summary>
+        /// <returns>Local configuration settings.</returns>
+        private static Dictionary<string, string> LoadWebConfig()
+        {
+            LogDebug.DebugContent(Assembly.GetExecutingAssembly().GetName().Name, Settings.Default.DebugMode, Settings.Default.DebugLogRoot, "[DEBUG] Loading configuration settings from Web.config.");
+
+            return new Dictionary<string, string>
+            {
+                { "DebugMode",              Properties.Settings.Default.DebugMode.ToLower() },
+                { "DebugLogRoot",           Properties.Settings.Default.DebugLogRoot.ToLower() },
+                { "AbatabMode",             Properties.Settings.Default.AbatabMode.ToLower() },
+                { "AbatabRoot",             Properties.Settings.Default.AbatabRoot.ToLower() },
+                { "LoggingMode",            Properties.Settings.Default.LoggingMode.ToLower() },
+                { "LoggingDetail",          Properties.Settings.Default.LoggingDetail.ToLower() },
+                { "LoggingDelay",           Properties.Settings.Default.LoggingDelay.ToLower() },
+                { "AvatarFallbackUserName", Properties.Settings.Default.AvatarFallbackUserName.ToLower() },
+                { "ModuleDateMode",         Properties.Settings.Default.ModuleDateMode.ToLower() },
+                { "ModuleDoseMode",         Properties.Settings.Default.ModuleDoseMode.ToLower() },
+                { "ModuleTestingMode",      Properties.Settings.Default.ModuleTestingMode.ToLower() }
+            };
+        }
+    }
+}
