9c51240ce3ce952554f3ab542a0d1b3ce2e56cf7
Chris Banwarth <github@aprettycoolprogram.com>
2022-10-24 09:15:22 -0400

    0.94.0-devbuild+221024.091417

--- src/Core/AbatabData/Core/OptionObject.cs
+++ src/Core/AbatabData/Core/OptionObject.cs
@@ -1,7 +1,7 @@
 ﻿// Abatab
 // Copyright (c) A Pretty Cool Program
 // See the LICENSE file for more information.
-// b221020.103236
+// b221024.091417
 
 namespace AbatabData.Core
 {
--- src/Libraries/Du/Du.cs
+++ src/Libraries/Du/Du.cs
@@ -1,7 +1,7 @@
 ﻿// Du v1.0
 // Copyright (c) A Pretty Cool Program
 // See the LICENSE file for more information.
-// b221020.103236
+// b221024.091417
 
 namespace Du
 {
