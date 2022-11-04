588b73833bab985db0023056ad06d216f6526643
Chris Banwarth <github@aprettycoolprogram.com>
2022-11-03 22:06:57 -0400

    0.96.0-devbuild+221103.220655

--- src/Abatab.asmx.cs
+++ src/Abatab.asmx.cs
@@ -44,8 +44,8 @@ namespace Abatab
         /// <returns>A finalized OptionObject.</returns>
         /// <remarks>
         /// * This method is required by Avatar.
-        /// * This is the only time a <see href="https://spectrum-health-systems.github.io/Abatab/articles/SourceCode/Logging.html#primevaldebug-log">PrimevalDebug log</see> is written.
-        /// * This method should remain fairly static, sicne most of the logic is taken care of by external projects.
+        /// * This is the only time a <see href="https://spectrum-health-systems.github.io/Abatab/articles/SourceCode/Logging.html#primevaldebug-log">PrimevalDebug</see> log is written.
+        /// * This method should remain fairly static, since most of the logic is taken care of by external projects.
         /// </remarks>
         [WebMethod]
         public OptionObject2015 RunScript(OptionObject2015 sentOptionObject, string scriptParameter)
