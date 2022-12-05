7fa6d1d6c1a304838174734bd8624637ea71a7d0
Chris Banwarth <github@aprettycoolprogram.com>
2022-12-05 12:41:49 -0500

    22.12.0-development+221205.1241

--- src/Abatab.asmx.cs
+++ src/Abatab.asmx.cs
@@ -1,7 +1,7 @@
 ﻿// Abatab 22.12.0
 // Copyright (c) A Pretty Cool Program
 // See the LICENSE file for more information.
-// b221205.1233
+// b221205.1241
 
 using Abatab.Properties;
 using AbatabData;
