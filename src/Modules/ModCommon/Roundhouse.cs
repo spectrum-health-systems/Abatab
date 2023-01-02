1eb2b8088760434b6f0e4d98a008a56ea8955e86
Chris Banwarth <github@aprettycoolprogram.com>
2023-01-02 12:07:27 -0500

    Refresh documentation

---
+++ src/Modules/ModCommon/Roundhouse.cs
@@ -0,0 +1,12 @@
+﻿// Abatab ModCommon 23.0.0
+// Copyright (c) A Pretty Cool Program
+// See the LICENSE file for more information.
+// b221228.1025
+
+namespace ModCommon
+{
+    /// <summary>TBD</summary>
+    public class Roundhouse
+    {
+    }
+}
