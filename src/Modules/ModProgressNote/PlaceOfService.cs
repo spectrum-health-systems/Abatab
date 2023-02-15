d51e1a14979ff0d29a6ccdd311ddcb7ae2510716
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-15 12:49:24 -0500

    v23.2.0-development+230215.1248

--- src/Modules/ModProgressNote/PlaceOfService.cs
+++ src/Modules/ModProgressNote/PlaceOfService.cs
@@ -116,6 +116,9 @@ namespace ModProgressNote
                 }
             }
 
+            LogEvent.Trace(abatabSession, Assembly.GetExecutingAssembly().GetName().Name, abatabSession.ModProgressNoteConfig.TelehealthConfig.ServiceChargeCodeValue);
+            LogEvent.Trace(abatabSession, Assembly.GetExecutingAssembly().GetName().Name, abatabSession.ModProgressNoteConfig.TelehealthConfig.LocationValue);
+
             if (abatabSession.ModProgressNoteConfig.TelehealthConfig.ValidServiceChargeCodes.Contains(abatabSession.ModProgressNoteConfig.TelehealthConfig.ServiceChargeCodeValue))
             {
                 if (abatabSession.ModProgressNoteConfig.TelehealthConfig.ValidLocations.Contains(abatabSession.ModProgressNoteConfig.TelehealthConfig.LocationValue))
