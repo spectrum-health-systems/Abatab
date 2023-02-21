30706403acc689c5a1815ad684ff8ad166486e83
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-21 12:42:49 -0500

    23.2.0-development+230221.1242

--- src/Modules/ModProgressNote/PlaceOfService.cs
+++ src/Modules/ModProgressNote/PlaceOfService.cs
@@ -174,6 +174,7 @@ namespace ModProgressNote
             {
                 // TODO This trace file should stay, and we might want to add a description to the msg.
                 LogEvent.Trace(abatabSession, Assembly.GetExecutingAssembly().GetName().Name, "[TRACE]");
+                LogEvent.TraceMsg(abatabSession, Assembly.GetExecutingAssembly().GetName().Name, fieldObject.FieldValue);
 
                 abatabSession.ModProgressNoteConfig.TelehealthConfig.LocationValue = "";
             }
