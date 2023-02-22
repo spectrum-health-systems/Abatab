54fe4c5b1593b643c39a6bb3aabf68225b349edb
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-22 17:28:02 -0500

    23.2.0-development+230222.1728

--- src/WebConfig.cs
+++ src/WebConfig.cs
@@ -25,23 +25,23 @@ namespace Abatab
 
             var test = new Dictionary<string, string>()
             {
-                { "AbatabMode",                             Settings.Default.AbatabMode.Trim() },
-                { "AbatabServiceRoot",                      Settings.Default.AbatabServiceRoot.Trim() },
-                { "AbatabDataRoot",                         Settings.Default.AbatabDataRoot.Trim() },
-                { "PublicAccessRoot",                       Settings.Default.PublicAccessRoot.Trim() },
-                { "LoggerMode",                             Settings.Default.LoggerMode.Trim() },
-                { "LoggerDelay",                            Settings.Default.LoggerDelay.Trim() },
-                { "AvatarEnvironment",                      Settings.Default.AvatarEnvironment.Trim() },
-                //{ "AbatabFallbackUserName",                 Settings.Default.AbatabFallbackUserName.Trim() },
-                //{ "ModProgressNoteMode",                    Settings.Default.ModProgressNoteMode.Trim() },
-                //{ "ModProgressNoteAuthorizedUsers",         Settings.Default.ModProgressNoteAuthorizedUsers.Trim() },
-                //{ "ModPrototypeMode",                       Settings.Default.ModPrototypeMode.Trim() },
-                //{ "ModQuickMedOrderMode",                   Settings.Default.ModQuickMedOrderMode.Trim() },
-                //{ "ModQuickMedOrderAuthorizedUsers",        Settings.Default.ModQuickMedOrderAuthorizedUsers.Trim()  },
-                //{ "ModQuickMedOrderValidOrderTypes",        Settings.Default.ModQuickMedOrderValidOrderTypes.Trim() },
-                //{ "ModQuickMedOrderDosePercentBoundary",    Settings.Default.ModQuickMedOrderDosePercentBoundary.Trim() },
-                //{ "ModQuickMedOrderDoseMilligramsBoundary", Settings.Default.ModQuickMedOrderDoseMilligramsBoundary.Trim() },
-                //{ "ModTestingMode",                         Settings.Default.ModTestingMode.Trim() }
+                //{ "AbatabMode",                             Settings.Default.AbatabMode.Trim() },
+                //{ "AbatabServiceRoot",                      Settings.Default.AbatabServiceRoot.Trim() },
+                //{ "AbatabDataRoot",                         Settings.Default.AbatabDataRoot.Trim() },
+                //{ "PublicAccessRoot",                       Settings.Default.PublicAccessRoot.Trim() },
+                //{ "LoggerMode",                             Settings.Default.LoggerMode.Trim() },
+                //{ "LoggerDelay",                            Settings.Default.LoggerDelay.Trim() },
+                //{ "AvatarEnvironment",                      Settings.Default.AvatarEnvironment.Trim() },
+                { "AbatabFallbackUserName",                 Settings.Default.AbatabFallbackUserName.Trim() },
+                { "ModProgressNoteMode",                    Settings.Default.ModProgressNoteMode.Trim() },
+                { "ModProgressNoteAuthorizedUsers",         Settings.Default.ModProgressNoteAuthorizedUsers.Trim() },
+                { "ModPrototypeMode",                       Settings.Default.ModPrototypeMode.Trim() },
+                { "ModQuickMedOrderMode",                   Settings.Default.ModQuickMedOrderMode.Trim() },
+                { "ModQuickMedOrderAuthorizedUsers",        Settings.Default.ModQuickMedOrderAuthorizedUsers.Trim()  },
+                { "ModQuickMedOrderValidOrderTypes",        Settings.Default.ModQuickMedOrderValidOrderTypes.Trim() },
+                { "ModQuickMedOrderDosePercentBoundary",    Settings.Default.ModQuickMedOrderDosePercentBoundary.Trim() },
+                { "ModQuickMedOrderDoseMilligramsBoundary", Settings.Default.ModQuickMedOrderDoseMilligramsBoundary.Trim() },
+                { "ModTestingMode",                         Settings.Default.ModTestingMode.Trim() }
             };
 
             return test;
