af6df2a4a91599949b3f111b51fad320da8dc278
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-25 12:30:39 -0500

    23.2.0-development+230225.1230

--- src/Flightpath.cs
+++ src/Flightpath.cs
@@ -3,8 +3,10 @@
 // Copyright (c) A Pretty Cool Program
 
 using System.Collections.Generic;
+using System.Reflection;
 using Abatab.Core.Catalog;
 using Abatab.Core.Session;
+using Abatab.Core.Utilities;
 using ScriptLinkStandard.Objects;
 
 namespace Abatab
@@ -13,6 +15,10 @@ namespace Abatab
     {
         public static void Starter(OptionObject2015 sentOptionObject, string scriptParameter, Dictionary<string, string> webConfigContent)
         {
+            /* For debugging only! Leave commented out in production environments!
+            */
+            Debuggler.WriteLocal(Assembly.GetExecutingAssembly().GetName().Name);
+
             SessionProperties sessionProperties = Build.NewSession(sentOptionObject, scriptParameter, webConfigContent);
 
             Roundhouse.ParseModule(sessionProperties);
