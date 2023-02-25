19be15ba2a1c30ca60f6f05d684b3c4c0dfe18db
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-25 13:45:31 -0500

    23.2.0-development+230225.1345

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Abatab.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -152,9 +152,9 @@ namespace Abatab.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("20")]
-        public string ModQuickMedicationOrderDoseMilligramsBoundary {
+        public string ModQuickMedicationOrderDoseMilligramBoundary {
             get {
-                return ((string)(this["ModQuickMedicationOrderDoseMilligramsBoundary"]));
+                return ((string)(this["ModQuickMedicationOrderDoseMilligramBoundary"]));
             }
         }
         
