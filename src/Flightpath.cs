e258be0d4b38c092a0b0ff20c9e77ca351d8d09b
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-27 09:18:07 -0500

    23.2.0-development+230227.0918

--- src/Flightpath.cs
+++ src/Flightpath.cs
@@ -19,7 +19,7 @@ namespace Abatab
         public static void Starter(OptionObject2015 sentOptionObject, string scriptParameter, AbSession abSession, Dictionary<string, string> webConfigContent)
         {
             Build.NewSession(sentOptionObject, scriptParameter, abSession, webConfigContent);
-            //Core.DataExport.SessionInformation.ToSessionRoot(abSession);
+
             if (!Directory.Exists(abSession.SessionDataRoot))
             {
                 Debuggler.WriteLocal(Assembly.GetExecutingAssembly().GetName().Name);
