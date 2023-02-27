8b2a60fa69599fb5922f8157444a8bab99e512c6
Chris Banwarth <github@aprettycoolprogram.com>
2023-02-27 10:38:50 -0500

    23.2.0-development+230227.1038

--- src/WebConfig.cs
+++ src/WebConfig.cs
@@ -15,26 +15,43 @@ namespace Abatab
         {
             Debuggler.WriteLocal(Assembly.GetExecutingAssembly().GetName().Name);
 
-            abSession.AbatabMode                                    = Settings.Default.AbatabMode;
-            abSession.AbatabVersion                                 = Settings.Default.AbatabVersion;
-            abSession.AbatabBuild                                   = Settings.Default.AbatabBuild;
-            abSession.AbatabServiceRoot                             = Settings.Default.AbatabServiceRoot;
-            abSession.AbatabDataRoot                                = Settings.Default.AbatabDataRoot;
-            abSession.LoggerMode                                    = Settings.Default.LoggerMode;
-            abSession.LoggerDelay                                   = Settings.Default.LoggerDelay;
-            abSession.LoggerTypes                                   = Settings.Default.LoggerTypes;
-            abSession.AvatarEnvironment                             = Settings.Default.AvatarEnvironment;
-            abSession.AbatabFallbackUserName                        = Settings.Default.AbatabFallbackUserName;
-            abSession.ModProgressNote.Mode                          = Settings.Default.ModProgressNoteMode;
-            abSession.ModProgressNote.AuthorizedUsers               = Settings.Default.ModProgressNoteAuthorizedUsers;
-            abSession.ModPrototype.Mode                             = Settings.Default.ModPrototypeMode;
-            abSession.ModPrototype.AuthorizedUsers                  = Settings.Default.ModPrototypeAuthorizedUsers;
-            abSession.ModQuickMedicationOrder.Mode                  = Settings.Default.ModQuickMedicationOrderMode;
-            abSession.ModQuickMedicationOrder.AuthorizedUsers       = Settings.Default.ModQuickMedicationOrderAuthorizedUsers;
-            abSession.ModQuickMedicationOrder.ValidOrderTypes       = Settings.Default.ModQuickMedicationOrderValidOrderTypes;
-            abSession.ModQuickMedicationOrder.DosePercentBoundary   = Settings.Default.ModQuickMedicationOrderDosePercentBoundary;
-            abSession.ModQuickMedicationOrder.DoseMilligramBoundary = Settings.Default.ModQuickMedicationOrderDoseMilligramBoundary;
-            abSession.ModTesting.Mode                               = Settings.Default.ModTestingMode;
+            abSession.AbatabMode             = Settings.Default.AbatabMode;
+            abSession.AbatabVersion          = Settings.Default.AbatabVersion;
+            abSession.AbatabBuild            = Settings.Default.AbatabBuild;
+            abSession.AbatabServiceRoot      = Settings.Default.AbatabServiceRoot;
+            abSession.AbatabDataRoot         = Settings.Default.AbatabDataRoot;
+            abSession.LoggerMode             = Settings.Default.LoggerMode;
+            abSession.LoggerDelay            = Settings.Default.LoggerDelay;
+            abSession.LoggerTypes            = Settings.Default.LoggerTypes;
+            abSession.AvatarEnvironment      = Settings.Default.AvatarEnvironment;
+            abSession.AbatabFallbackUserName = Settings.Default.AbatabFallbackUserName;
+
+            abSession.ModProgressNote = new ModProgressNote
+            {
+                Mode            = Settings.Default.ModProgressNoteMode,
+                AuthorizedUsers = Settings.Default.ModProgressNoteAuthorizedUsers
+            };
+
+            abSession.ModPrototype = new ModPrototype
+            {
+                Mode            = Settings.Default.ModPrototypeMode,
+                AuthorizedUsers = Settings.Default.ModPrototypeAuthorizedUsers
+            };
+
+            abSession.ModQuickMedicationOrder = new ModQuickMedicationOrder
+            {
+                Mode                  = Settings.Default.ModQuickMedicationOrderMode,
+                AuthorizedUsers       = Settings.Default.ModQuickMedicationOrderAuthorizedUsers,
+                ValidOrderTypes       = Settings.Default.ModQuickMedicationOrderValidOrderTypes,
+                DosePercentBoundary   = Settings.Default.ModQuickMedicationOrderDosePercentBoundary,
+                DoseMilligramBoundary = Settings.Default.ModQuickMedicationOrderDoseMilligramBoundary
+            };
+
+            abSession.ModTesting = new ModTesting
+            {
+                Mode = Settings.Default.ModTestingMode
+            };
+
             abSession.DebugglerMode                                 = Settings.Default.DebugglerMode;
 
             Debuggler.WriteLocal(Assembly.GetExecutingAssembly().GetName().Name);
