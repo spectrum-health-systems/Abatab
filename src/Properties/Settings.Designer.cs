ef28a5851dbc10eacaa1b74d63e76bb9b98053ea
Chris Banwarth <github@aprettycoolprogram.com>
2023-03-30 13:30:39 -0400

    Development build 230330.1330

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -205,7 +205,7 @@ namespace Abatab.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("230330")]
+        [global::System.Configuration.DefaultSettingValueAttribute("230330.1329")]
         public string AbatabBuild {
             get {
                 return ((string)(this["AbatabBuild"]));
