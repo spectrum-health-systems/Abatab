29f5c2070fc667a8c81c5f44f5255903aa8a63c4
Chris Banwarth <github@aprettycoolprogram.com>
2023-05-23 11:22:33 -0400

    Update

--- src/Properties/Settings.Designer.cs
+++ src/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Abatab.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -205,7 +205,7 @@ namespace Abatab.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("development+230515.1033")]
+        [global::System.Configuration.DefaultSettingValueAttribute("development+230523.1121")]
         public string AbatabBuild {
             get {
                 return ((string)(this["AbatabBuild"]));
