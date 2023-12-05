c154e576f7a18bb6fd925dd53079674e6940096e
Chris Banwarth <github@aprettycoolprogram.com>
2023-12-05 13:02:06 -0500

    b231205

--- src/Abatab/Abatab.asmx.cs
+++ src/Abatab/Abatab.asmx.cs
@@ -38,6 +38,9 @@ namespace Abatab
         public string GetVersion() => "VERSION 23.12";
 
         /// <summary>The starting point for Abatab.</summary>
+        /// <param name="sentOptionObject">The OptionObject sent from myAvatar.</param>
+        /// <param name="scriptParameter">The Script Parameter from myAvatar.</param>
+        /// <returns>The finalized OptionObject to myAvatar.</returns>
         [WebMethod]
         public OptionObject2015 RunScript(OptionObject2015 sentOptionObject,string scriptParameter)
         {
@@ -63,6 +66,8 @@ namespace Abatab
     }
 }
 
+
+/// <summary>The starting point for Abatab.</summary>
 /// <summary>The starting point for Abatab.</summary>
 /// <param name="sentOptionObject">The OptionObject sent from myAvatar.</param>
 /// <param name="scriptParameter">The Script Parameter from myAvatar.</param>
